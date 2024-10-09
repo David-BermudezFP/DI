53046ad3e31af0f8359c10feb68d8f20b4bf6512
David-BermudezFP <dbermudezl24@fpcoruna.afundacion.org>
2024-10-09 09:34:17 +0200

    Ejercicio 1

---
+++ sprint0python/pythonProject/Scripts/operaciones.py
@@ -0,0 +1,32 @@
+#PROCEDURAL
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+def multiplicacion(a, b):
+    return a * b
+
+def division(a, b):
+    if b == 0:
+        return "Error: División por cero no es permitida"
+    return a / b
+
+#Orientado a objetos
+#
+# class Operaciones:
+#
+#     def suma(self, a, b):
+#         return a + b
+#
+#     def resta(self, a, b):
+#         return a - b
+#
+#     def multiplicacion(self, a, b):
+#         return a * b
+#
+#     def division(self, a, b):
+#         if b == 0:
+#             return "Error: División por cero no es permitida"
+#         return a / b
