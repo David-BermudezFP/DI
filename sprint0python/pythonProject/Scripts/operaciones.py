825d161fc12501e80416bb6b29a9ffb9560c666a
David-BermudezFP <dbermudezl24@fpcoruna.afundacion.org>
2024-10-11 21:09:50 +0200

    Añadidos comentarios al Ejercicio 1 (operaciones.py)

--- sprint0python/pythonProject/Scripts/operaciones.py
+++ sprint0python/pythonProject/Scripts/operaciones.py
@@ -1,19 +1,26 @@
-#PROCEDURAL
+#Realización de forma procedural
+
+# Función para sumar dos números
 def suma(a, b):
-    return a + b
+    return a + b  # Retorna la suma de 'a' y 'b'
 
+# Función para restar dos números
 def resta(a, b):
-    return a - b
+    return a - b  # Retorna la resta de 'b' de 'a'
 
+# Función para multiplicar dos números
 def multiplicacion(a, b):
-    return a * b
+    return a * b  # Retorna el producto de 'a' y 'b'
 
+# Función para dividir dos números
 def division(a, b):
+    # Verifica que 'b' no sea cero para evitar división por cero
     if b == 0:
         return "Error: División por cero no es permitida"
-    return a / b
+    return a / b  # Retorna el resultado de 'a' dividido por 'b'
 
-#Orientado a objetos
+
+# Realizacion de forma orientada a objetos
 #
 # class Operaciones:
 #
